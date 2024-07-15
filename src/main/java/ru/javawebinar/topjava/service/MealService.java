c79ec23a2a200446cd60c437035817d3b99544f0
marchstein <goodsala@mail.ru>
2024-07-15 21:33:29 +0600

    2_4_add_spring

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.service;
 
 import ru.javawebinar.topjava.repository.MealRepository;
 
-public class MealService {
+public class    MealService {
 
     private MealRepository repository;
 
